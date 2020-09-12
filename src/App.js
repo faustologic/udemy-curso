0e97c4c13a3517c474d929b8131b05654a66d9b2
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-11 22:19:12 -0500

    ESLint
    creamos un archivo .eslintrc en el root de la app.

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>ReactJS</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido al curso de ReactJS en Udemy!
         </p>
         <a
           className="App-link"
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
