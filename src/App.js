cc39e00e252e2be8bb9595a00a4d94eccd6feac8
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-12 13:36:05 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header/Header';
+
+class Hello extends Component {
+  // Se importa desde la primera línea
+  render() {
+    return <h2>{this.props.title}</h2>
+  }
+}
+
+class Text extends Component {
+  render() {
+    const textoSegunBool = this.props.boolean ? "Es cierto" : "Falso";
+    return (
+      <div>
+        <p>{this.props.text}</p>
+        <p>{this.props.number}</p>
+        <p>{textoSegunBool}</p>
+        <p>{this.props.arrayOfNumbers.join(", ")}</p>
+      </div>
+    )
+  }
+}
 
 function App() {
+
+  //const Hello = (props) => <h2>{props.title}</h2>
+  // Hello es una constante que tiene su propiedad, se coloca en el return de manera JSX utilizando como componente y agregando el valor del titulo ya que es su propiedad
+
   return (
     <div className="App">
-      <Header />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Hello 
+          title = "Hello from props" 
+        />
+        <Text 
+          arrayOfNumbers = {[4,6,7,8,3,1]}
+          boolean
+          text="Texto en String"
+          number={3}
+        />
+      </header>
     </div>
   );
 }
